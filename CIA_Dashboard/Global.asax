7c8dd874125a530128b9d89962ae3eab1ca45629
jvitulli <jvitulli@consumer.org>
2019-04-19 11:27:24 -0400

    Add project files.

---
+++ CIA_Dashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="CIA_Dashboard.Global_asax" Language="vb" %>
---
+++ CIA_Dashboard/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("CIA_Dashboard")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Consumer Reports")>
+<Assembly: AssemblyProduct("CIA_Dashboard")>
+<Assembly: AssemblyCopyright("Copyright © Consumer Reports 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("79e8b569-cf1c-448e-87ab-ed7387336359")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
